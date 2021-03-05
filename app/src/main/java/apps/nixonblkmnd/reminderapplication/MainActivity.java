ea9d3d61ea1b7882765bc948e3bd7cbb74640bd2
Fletcher0132 <fletchermoore2310@gmail.com>
2021-03-05 23:14:06 +0000

    MainPage, colour, layout edits

--- app/src/main/java/apps/nixonblkmnd/reminderapplication/MainActivity.java
+++ app/src/main/java/apps/nixonblkmnd/reminderapplication/MainActivity.java
@@ -2,15 +2,33 @@ package apps.nixonblkmnd.reminderapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Buttons
+    private Button btnCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalendar = (Button) findViewById(R.id.btnCalendar);
+        btnCalendar.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                navReminderCalendar();
+            }
+        });
+    }
+
+    public void navReminderCalendar() {
+        Intent intent = new Intent(this, ReminderCalendar.class);
+        startActivity(intent);
     }
 }
