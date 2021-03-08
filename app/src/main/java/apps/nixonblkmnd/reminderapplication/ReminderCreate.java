70e05c6f4183603a31a2564f8b7065f5b94b8281
Fletcher0132 <fletchermoore2310@gmail.com>
2021-03-08 21:03:23 +0000

    ReminderCreate, validating and storing input

--- app/src/main/java/apps/nixonblkmnd/reminderapplication/ReminderCreate.java
+++ app/src/main/java/apps/nixonblkmnd/reminderapplication/ReminderCreate.java
@@ -2,13 +2,32 @@ package apps.nixonblkmnd.reminderapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ReminderCreate extends AppCompatActivity {
+public class ReminderCreate extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_create);
+
+        //BUTTON
+        Button btnAddReminder = findViewById(R.id.btnAddReminder);
+
+        //BUTTON ONCLICK
+        btnAddReminder.setOnClickListener(this);
+    }
+
+
+    //VALIDATE INPUT AND STORE TO DISPLAY REMINDER AND NOTIFY USER
+    //THEN RETURN TO MAIN-PAGE/CALENDAR
+    @Override
+    public void onClick(View v) {
+        //SET VARIABLES
+
+
     }
 }
