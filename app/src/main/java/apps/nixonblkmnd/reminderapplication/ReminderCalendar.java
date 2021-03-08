d827ac6f65497ac9cdb36c2f494c9095a60293c9
Fletcher0132 <fletchermoore2310@gmail.com>
2021-03-08 11:54:49 +0000

    ReminderCalendar, calendar implemented, button created, button navigates

--- app/src/main/java/apps/nixonblkmnd/reminderapplication/ReminderCalendar.java
+++ app/src/main/java/apps/nixonblkmnd/reminderapplication/ReminderCalendar.java
@@ -2,13 +2,31 @@ package apps.nixonblkmnd.reminderapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ReminderCalendar extends AppCompatActivity {
+public class ReminderCalendar extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_calendar);
+
+
+        //BUTTON
+        Button btnCreateRemCal = findViewById(R.id.btnCreateRemCal);
+
+        //BUTTON ONCLICK
+        btnCreateRemCal.setOnClickListener(this);
+    }
+
+
+    //NAVIGATE ONCLICK
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ReminderCreate.class);
+        startActivity(intent);
     }
 }
