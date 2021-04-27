89241395eb6a646e525fb5b11b2ffd8854693fe5
Fletcher0132 <fletchermoore2310@gmail.com>
2021-04-27 21:25:11 +0100

    ReminderMap, current user location working. Working on upcoming reminders for location-based events

--- app/src/main/java/apps/nixonblkmnd/reminderapplication/ReminderMap.java
+++ app/src/main/java/apps/nixonblkmnd/reminderapplication/ReminderMap.java
@@ -1,48 +1,123 @@
 package apps.nixonblkmnd.reminderapplication;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class ReminderMap extends FragmentActivity implements OnMapReadyCallback {
+import static android.Manifest.permission;
+import static android.content.ContentValues.TAG;
+import static apps.nixonblkmnd.reminderapplication.R.id.txtMapUpcoming;
+
+public class ReminderMap extends FragmentActivity {
 
     private GoogleMap mMap;
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //INITIALIZE FUSED LOCATION
+        client = LocationServices.getFusedLocationProviderClient(this);
         Places.initialize(this, BuildConfig.GMP_KEY);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        ListView mapUpcoming = (ListView) findViewById(txtMapUpcoming);
+
+        //VIEW UPCOMING REMINDERS
+        ViewReminders();
+
+        //METHOD FOR USER LOCATION
+        getCurrentLocation(mapFragment);
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    //VIEW THE REMINDERS UPCOMING BASED ON LOCATION
+    public void ViewReminders(){
+        try {
+
+        }catch (Exception e){
+            Log.e(TAG,"Error displaying upcoming Map reminders");
+        }
     }
+
+
+    //RETRIEVE USER LOCATION AFTER PERMISSION CHECK
+    public void getCurrentLocation(SupportMapFragment mapFragment) {
+        Dexter.withContext(this).withPermission(permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                if ((ActivityCompat.checkSelfPermission(ReminderMap.this, permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(ReminderMap.this, "Location Permission not Enabled. Enable via Settings", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Task<Location> task = client.getLastLocation();
+                task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        //SUCCESS
+                        if (location != null) {
+                            //USER LOCATION
+                            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                                @Override
+                                public void onMapReady(GoogleMap googleMap) {
+                                    //GET LOCATION
+                                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                    //GET MARKER OPTIONS
+                                    MarkerOptions marker = new MarkerOptions().position(latLng);
+                                    //FIX ZOOM - OTHERWISE TOO FAR OUT
+                                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                                    //SET MARKER LOCATION
+                                    googleMap.addMarker(marker);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+
 }
